60a8d525fc2442ac79c39f38fca6783c766591d7
Sean Jewell <sean@isgoms.com>
2023-03-10 17:36:11 +0000

    Interesting. So basically the box is drawn relative to the adjusted BB of the model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import React, { useRef, useState, useEffect } from 'react'
-import { BoxHelper } from 'three';
+import { Vector3, Ray, MathUtils, BoxHelper, Box3, BoxGeometry, Matrix4, Mesh, MeshBasicMaterial } from 'three';
 import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
@@ -11,19 +11,47 @@ import './index.css';
 function Model(props) {
 
   const geometry = useLoader(STLLoader, './Head.stl');
+  const [boxGeo, setBoxGeo ] = useState(null);
 
   // This reference will give us direct access to the mesh
   const mesh = useRef()
+  const boxMesh = useRef();
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-
-  // useFrame((state, delta) => (mesh.current.rotation.x += delta))
   useHelper(mesh, BoxHelper, 'cyan')
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += delta;
+  })
+
+  useEffect(() => {
+    // create your box mesh here
+    if(mesh){
+      console.log(mesh);
+      const box3 = new Box3();
+      box3.setFromObject(mesh.current);
+
+      // make a BoxBufferGeometry of the same size as Box3
+      const dimensions = new Vector3().subVectors( box3.max, box3.min );
+      const boxGeo = new BoxGeometry(dimensions.x, dimensions.y, dimensions.z);
+
+      // move new mesh center so it's aligned with the original object
+      const matrix = new Matrix4().setPosition(dimensions.addVectors(box3.min, box3.max).multiplyScalar( 0.5 ));
+      boxGeo.applyMatrix4(matrix);
+      const myMesh = new Mesh(boxGeo, new MeshBasicMaterial( { color: 'red', wireframe: true } ));
+      mesh.current.add(myMesh);
+    }
+
+  }, [mesh.current]);
+
+
+
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <>
+
       <mesh
         {...props}
         ref={mesh}
@@ -34,10 +62,6 @@ function Model(props) {
         <primitive object={geometry} attach="geometry" />
         <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
       </mesh>
-      {active && <TransformControls
-          mode="rotate"
-          object={ mesh }
-      />}
     </>
   )
 }
