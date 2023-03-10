b951e161955f2ad03e1e7dff2be0ad78befd26a2
Sean Jewell <sean@isgoms.com>
2023-03-10 15:01:58 +0000

    basic demo of error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState, useEffect } from 'react'
+import { BoxHelper } from 'three';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+import { OrbitControls, useHelper, TransformControls } from '@react-three/drei';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Model(props) {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const geometry = useLoader(STLLoader, './Head.stl');
+
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+
+  // useFrame((state, delta) => (mesh.current.rotation.x += delta))
+  useHelper(mesh, BoxHelper, 'cyan')
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <>
+      <mesh
+        {...props}
+        ref={mesh}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <primitive object={geometry} attach="geometry" />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+      {active && <TransformControls
+          mode="rotate"
+          object={ mesh }
+      />}
+    </>
+  )
+}
+
+function Experience(props){
+  const camera = useRef();
+
+  return (
+    <Canvas>
+      <OrbitControls makeDefault ref={ camera } position={ [50, 50, 50 ] }/>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Model position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <Experience />
+)
